a7b26d3c9c0351030b0729053e8b3472e3801891
Shakti Kc <shakti3462@gmail.com>
2022-06-15 14:07:22 +0545

    working on contact demo

--- src/pages/ContactUs/components/ContactInfo.jsx
+++ src/pages/ContactUs/components/ContactInfo.jsx
@@ -4,36 +4,35 @@ import { useContactApi } from "../useContactApi";
 
 export const ContactInfo = () => {
   const { getContactData } = useContactApi();
+  // console.log("getContactData", getContactData);
   return (
-    <Flex direction={"column"} textAlign="end">
-      <Text lineHeight="2rem">{getContactData?.footerInfo?.site_name}</Text>
-      <Text lineHeight="2rem">
-        {getContactData?.footerInfo?.location1}
+    <Box>
+      {" "}
+      <Flex direction={"column"} textAlign="end">
+        <Text lineHeight="2rem">{getContactData?.site_name}</Text>
+        <Text lineHeight="2rem">
+          {getContactData?.location1}
 
-        {getContactData?.footerInfo?.location2 !== "null" &&
-          getContactData?.footerInfo?.location2}
-      </Text>
-      <Text lineHeight="2rem">
-        {getContactData?.footerInfo?.contact1}
-        {getContactData?.footerInfo?.contact2 !== "null" &&
-          getContactData?.footerInfo?.contact2}
-      </Text>
-      <Text lineHeight="2rem">
-        {getContactData?.footerInfo?.email1}
-        <Box>
-          {getContactData?.footerInfo?.email2 !== "null" &&
-            getContactData?.footerInfo?.email2}
-        </Box>
-      </Text>
-      <Text lineHeight="2rem">
-        <Link to={getContactData?.footerInfo?.facebook_link}>
-          {getContactData?.footerInfo?.facebook_link}
-        </Link>
-      </Text>
-      <Text lineHeight="2rem">{getContactData?.footerInfo?.youtube_link}</Text>
-      <Text lineHeight="2rem">
-        {getContactData?.footerInfo?.instagram_link}
-      </Text>
-    </Flex>
+          {getContactData?.location2 !== "null" && getContactData?.location2}
+        </Text>
+        <Text lineHeight="2rem">
+          {getContactData?.contact1}
+          {getContactData?.contact2 !== "null" && getContactData?.contact2}
+        </Text>
+        <Text lineHeight="2rem">
+          {getContactData?.email1}
+          <Box>
+            {getContactData?.email2 !== "null" && getContactData?.email2}
+          </Box>
+        </Text>
+        <Text lineHeight="2rem">
+          <Link to={getContactData?.facebook_link}>
+            {getContactData?.facebook_link}
+          </Link>
+        </Text>
+        <Text lineHeight="2rem">{getContactData?.youtube_link}</Text>
+        <Text lineHeight="2rem">{getContactData?.instagram_link}</Text>
+      </Flex>
+    </Box>
   );
 };
