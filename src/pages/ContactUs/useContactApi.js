c7ffe2d7b328a1fe4f3850d90b88fc4550f2fd17
Shakti Kc <shakti3462@gmail.com>
2022-06-15 11:30:51 +0545

    working on project setup

---
+++ src/pages/ContactUs/useContactApi.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const useContactApi = () => {
+  return <div>useContactApi</div>;
+};
---
+++ src/pages/ContactUs/useContactUs.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const useContactUs = () => {
+  return <div>useContactUs</div>;
+};
